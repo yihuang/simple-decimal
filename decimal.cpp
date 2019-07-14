0e8e5bd4547f898a951d50576549647f084e4746
yihuang <yi.codeplayer@gmail.com>
2019-07-15 02:47:30 +0800

    add operation

--- decimal.cpp
+++ decimal.cpp
@@ -2,6 +2,7 @@
 
 #define PRECISION_LIMIT 19
 #define MAX_BUF_DECIMAL 44
+#define MAX_INT64_DIGITS (20+2)
 
 static const uint64_t POW10[] = {
     1,
@@ -106,6 +107,14 @@ bool Decimal::operator ==(const Decimal& other) const {
     }
 }
 
+bool Decimal::operator ==(uint64_t other) const {
+    if (_prec == 0) {
+        return _n == other;
+    } else {
+        return _n * POW10[_prec] == other;
+    }
+}
+
 bool Decimal::operator <(const Decimal& other) const {
     if (_prec == other._prec) {
         return _n < other._n;
@@ -172,7 +181,7 @@ bool Decimal::set_string(const char* s, size_t len) {
         if (s[i] == '.') {
             fractional = false;
         } else {
-            if (s[i] >= '0' && s[i] <= '9') {
+            if (s[i] > '0' && s[i] <= '9') {
                 _n += (s[i] - '0') * POW10[digits];
             } else if (s[i] != '0') {
                 break;
@@ -192,5 +201,106 @@ bool Decimal::set_string(const char* s, size_t len) {
         }
     }
 
+    return true;
+}
+
+uint128_t multiple(uint64_t a, unsigned a_exp, uint64_t b, unsigned b_exp, unsigned *o_exp) {
+    *o_exp = a_exp + b_exp;
+    return (uint128_t)a * (uint128_t)b;
+}
+
+#define DIGITS_LIMIT_64BIT 20
+bool parse_decimal(const char* s, size_t len, unsigned exp_limit, uint64_t* _n) {
+    // parse unsigned decimal with exp_limit scale.
+    uint64_t n = 0;
+    int i;
+    int exp = 0;
+    bool met_dot = false;  // had we met dot?
+    int digits = 0;
+
+    if (len > MAX_INT64_DIGITS) {
+        // overflow
+        return false;
+    }
+    if (len > 0 && s[0] == '-') {
+        return false;
+    }
+    for (i=(int)len-1; i>=0; i--) {
+        if (s[i] == '.') {
+            met_dot = true;
+        } else {
+            if (s[i] > '0' && s[i] <= '9') {
+                n += (s[i] - '0') * POW10[digits];
+            } else if (s[i] != '0') {
+                // invalid character
+                break;
+            }
+            digits++;
+            if (digits == DIGITS_LIMIT_64BIT) {
+                // may overflow
+                return false;
+            }
+            if (!met_dot) {
+                exp++;
+            }
+        }
+    }
+    if (i >= 0 && s[i] != '+') {
+        // invalid character
+        return false;
+    }
+
+    exp = met_dot ? exp : 0;
+    if (exp > exp_limit) {
+        return false;
+    } else {
+        *_n = n * POW10[exp_limit - exp];
+    }
+    return true;
+}
+
+bool parse_decimal(const char* s, size_t len, unsigned *_exp, uint64_t* _n) {
+    // parse unsigned decimal
+    uint64_t n = 0;
+    int i;
+    int exp = 0;
+    bool met_dot = false;  // had we met dot?
+    int digits = 0;
+
+    if (len > MAX_INT64_DIGITS) {
+        // overflow
+        return false;
+    }
+    if (len > 0 && s[0] == '-') {
+        return false;
+    }
+    for (i=(int)len-1; i>=0; i--) {
+        if (s[i] == '.') {
+            met_dot = true;
+        } else {
+            if (s[i] > '0' && s[i] <= '9') {
+                n += (s[i] - '0') * POW10[digits];
+            } else if (s[i] != '0') {
+                // invalid character
+                break;
+            }
+            digits++;
+            if (digits == DIGITS_LIMIT_64BIT) {
+                // may overflow
+                return false;
+            }
+            if (!met_dot) {
+                exp++;
+            }
+        }
+    }
+    if (i >= 0 && s[i] != '+') {
+        // invalid character
+        return false;
+    }
+
+    exp = met_dot ? exp : 0;
+    *_exp = exp;
+    *_n = n;
     return true;
 }
--- decimal.h
+++ decimal.h
@@ -35,8 +35,17 @@ public:
 
     Decimal operator %(const Decimal& other) const;
     bool operator ==(const Decimal& other) const;
-    bool operator <(const Decimal& other) const;
+    bool operator ==(uint64_t other) const;
 
+    bool operator !=(const Decimal& other) const {
+        return !(*this == other);
+    }
+
+    bool operator !=(uint64_t other) const {
+        return !(*this == other);
+    }
+
+    bool operator <(const Decimal& other) const;
     bool operator <=(const Decimal& other) const {
         return *this==other || *this < other;
     }
@@ -69,3 +78,52 @@ public:
     int_type _n;
     uint16_t _prec;
 };
+
+bool parse_decimal(const char* s, size_t len, unsigned exp_limit, uint64_t* _n);
+bool parse_decimal(const char* s, size_t len, unsigned *_exp, uint64_t* _n);
+
+template<typename int_type>
+char* format_decimal(char *buf, size_t size, int_type _n, unsigned exp) {
+    char *p = buf+size-1;  // work from end
+    bool sign = _n >= 0;
+    bool remove_trailing_zero = exp > 0;
+    int_type n = sign ? _n : -_n;
+    size_t counter = 0;
+
+    *(p--) = '\0';
+    if (n == 0) {
+        *(p--) = '0';
+        return p+1;
+    }
+
+    while (n != 0 || counter <= exp) {
+        if (counter++ == exp) {
+            if (exp > 0) {
+                if (!remove_trailing_zero) {
+                    *(p--) = '.';
+                }
+                remove_trailing_zero = false;
+                continue;
+            }
+        }
+        if (n != 0) {
+            unsigned char c = (n % 10);
+            if (c == 0 && remove_trailing_zero) {
+            } else {
+                remove_trailing_zero = false;
+                *(p--) = '0' + c;
+            }
+            n /= 10;
+        } else {
+            *(p--) = '0';
+        }
+    }
+    if (exp > 0 && counter == exp + 1) {
+        *(p--) = '0';
+    }
+    if (!sign) {
+        *(p--) = '-';
+    }
+    return p+1;
+}
+
